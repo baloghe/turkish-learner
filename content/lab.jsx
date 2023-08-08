2a092eb225395bfbb84f668b59ea7bdfcee1c348
Endre BALOGH <balogh_endre@gmail.hu>
2023-08-08 13:13:00 +0200

    rotation bug fixed

--- content/lab.jsx
+++ content/lab.jsx
@@ -5,7 +5,7 @@ const cards = [
 ,{caption: '\u2551', up: true, down: true, left: false, right: false, turnCW: '\u2550', turnCCW: '\u2550'}
 ,{caption: '\u2554', up: false, down: true, left: false, right: true, turnCW: '\u2557', turnCCW: '\u255A'}
 ,{caption: '\u2557', up: false, down: true, left: true, right: false, turnCW: '\u255D', turnCCW: '\u2554'}
-,{caption: '\u255A', up: true, down: false, left: false, right: true, turnCW: '\u2557', turnCCW: '\u255D'}
+,{caption: '\u255A', up: true, down: false, left: false, right: true, turnCW: '\u2554', turnCCW: '\u255D'}
 ,{caption: '\u255D', up: true, down: false, left: true, right: false, turnCW: '\u255A', turnCCW: '\u2557'}
 ,{caption: '\u2560', up: true, down: true, left: false, right: true, turnCW: '\u2566', turnCCW: '\u2569'}
 ,{caption: '\u2563', up: true, down: true, left: true, right: false, turnCW: '\u2569', turnCCW: '\u2566'}
