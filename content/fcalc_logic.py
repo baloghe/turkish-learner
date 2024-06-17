d4ff95217259f3b71c48da82d4fca9f007366018
Endre BALOGH <balogh_endre@gmail.hu>
2024-06-17 14:35:46 +0200

    numbers instead of digits, spec cases + tests added

--- content/fcalc_logic.py
+++ content/fcalc_logic.py
@@ -34,6 +34,7 @@ symbols = {
   ,"SQ": {"k":"SQ","p":4,"s":"&sqrt;","t":"fn"}
   ,"DC": {"k":"DC","p":0,"s":".","t":"dig"}
   ,"NUMBER": {"k":"NUMBER","p":0,"s":"","t":"number"}
+  ,"EQU": {"k":"EQU","p":10,"s":"=","t":"equ"}
 }
 
 # further helpers
@@ -78,41 +79,66 @@ def reset():
     ,"OB": True, "CB": False
     ,"MU": False, "DI": False, "PL": False, "MI": False
     ,"PO": False, "LN": True, "SQ": True, "DC": True
+    ,"EQU": False
     ,"RESULT": ""
     ,"EXPRESSION": ""
     ,"STATE": "type something!"
   }
 
-def add_symbol(sym):
-  global state
+def add_symbol(sym, numliteral):
+  global state, symlist
   
-  print(f"addsymbol: {sym}:p{symbols[sym]['p']}")
+  # print(f"addsymbol: {sym}:p{symbols[sym]['p']}, lit: {numliteral}")
   
-  # reject unexpected symbol and quit
-  if not state[sym]:
-    print(f"  unexpected symbol, do nothing")
+  # tolerate (and correct) some malformed expressions
+  append_sym = True
+  if sym != "NUMBER" and symbols[sym]["t"] == "fn" and state["RESULT"] != "":
+    # we assume the User wants to evaluate this function with the previous result
+    # FN ( . ) added
+    symlist.insert(0, get_symbol('OB'))
+    symlist.insert(0, get_symbol(sym))
+    symlist.append(get_symbol('CB'))
+    append_sym = False
+    print(f"add_symbol spec #1 : {symlist_to_string(symlist)}")
+  if sym == "CB" and state["RESULT"] != "":
+    # we assume the User wants to put the previous expression into brackets
+    # ( . ) added
+    symlist.insert(0, get_symbol('OB'))
+    symlist.append(get_symbol('CB'))
+    append_sym = False
+    print(f"add_symbol spec #2 : {symlist_to_string(symlist)}")
+  elif sym != "NUMBER" and symbols[sym]["t"] != "fn" and not state[sym]:
+    # no idea what the user might have wanted
+    print(f"  unexpected symbol {sym}, do nothing")
     return
   
-  # add to operational + eventually bracket stack
-  handle_symbol(sym)
+  # add to the operational (+ eventually bracket) stack
+  expr_res = None # {state, value}
+  if append_sym:
+    expr_res = handle_symbol(sym, numliteral)
+  else:
+    expr_res = eval_symlist()
+    
+  # print(f"  expr_res: {expr_res}")
     
   # expression
   state["EXPRESSION"] = symlist_to_string(symlist)
     
-  # decide on state
+  # decide on button states
   calc_state()
-  print_state()
   
   # find out if a result is available
-  r = eval_stack()
-  if r["state"] == "empty":
+  if expr_res["state"] == "empty":
     state["RESULT"] =""
     state["STATE"] = "type something!"
-  elif r["state"] == "error":
+  elif expr_res["state"] == "malformed expression":
     state["RESULT"] =""
-    state["STATE"] = "complete the expression!"
+    state["STATE"] = "complete/modify the expression!"
+  elif expr_res["state"] == "unexpected value during evaluation":
+    state["RESULT"] =""
+    state["STATE"] = expr_res["state"]
   else:
-    state["RESULT"] = r["value"]
+    state["RESULT"] = expr_res["value"]
     state["STATE"] = "result"
   
 def enable_symbol(symarr):
@@ -129,10 +155,10 @@ def handle_brackets(sym):
   global brackets
   if len(brackets) > 0 and  brackets[-1] == "OB" and sym == "CB":
     brackets.pop()
-    print(f"brackets: top OB removed -> len={len(brackets)}")
+    # print(f"brackets: top OB removed -> len={len(brackets)}")
   else:
     brackets.append(sym)
-    print(f"brackets: append {sym} -> len={len(brackets)}")
+    # print(f"brackets: append {sym} -> len={len(brackets)}")
   
 def get_symbol(sym):
   return { "k":symbols[sym]["k"]
@@ -141,7 +167,7 @@ def get_symbol(sym):
           ,"t":symbols[sym]["t"]
   }
   
-def handle_symbol(sym):
+def handle_symbol(sym, numliteral):
   global opcount, dccount, symlist
   
   sss = symbols[sym]
@@ -150,25 +176,34 @@ def handle_symbol(sym):
   if sss["t"] == "bracket":
     handle_brackets(sym)
     
-  # operational stack
+  # expand symlist
   elem = None
   if sss["t"] == "dig":
+    # new number
     if len(symlist) == 0 or symlist[-1]["k"] != "NUMBER":
       elem = get_symbol("NUMBER")
-    else:
+    else: # continue last number
       elem = symlist.pop()
     elem["s"] += sss["s"]
     if sss["k"] == "DC":
       dccount = 1
+  elif sss["t"] == "number":
+    elem = get_symbol("NUMBER")
+    elem["s"] = numliteral
   else:
+    # simply add to the end and reset the dec.sep. counter
     elem = get_symbol(sym)
     dccount = 0
   
   symlist.append(elem)
+  # print(f"handle_symbol: added {elem}")
   
   # operation counter
   if sss["t"] == "fn" or sss["t"] == "inop":
     opcount += 1
+    
+  # evaluate new symlist
+  return eval_symlist() # {state, value}
   
 def copy_stack():
   ret = []
@@ -188,6 +223,7 @@ def to_polish_form(slist):
   ptr = 0
   while ptr < len(slist) and error == 0:
     act = slist[ptr]
+    # print(f"to_polish_form :: act={act}")
     if act["t"] == "number":
       polform.append(act)
     else:
@@ -213,7 +249,7 @@ def to_polish_form(slist):
             top = None
           
         opstack.append(act)
-    #print(f"  opstack: {symlist_to_string(opstack)}  ;  pf={symlist_to_string(polform)}")
+    # print(f"to_polish_form: {symlist_to_string(opstack)}  ;  pf={symlist_to_string(polform)}")
     ptr += 1
   
   while len(opstack) > 0:
@@ -237,16 +273,20 @@ def eval_inop(fn, arglist):
     return arglist[0] - arglist[1]
   elif fn == "MU" and len(arglist) > 1:
     return arglist[0] * arglist[1]
-  elif fn == "DI" and len(arglist) > 1 and arglist[1] != 0:
+  elif fn == "DI" and len(arglist) > 1:
     return arglist[0] / arglist[1]
   elif fn == "PO" and len(arglist) > 1:
     return math.pow(arglist[0] , arglist[1])
   else:
     return None
 
+class MalformedExpression(Exception):
+    pass
+
 def eval_polish_form(pf):
   args = []
   ptr = 0
+  
   while ptr < len(pf):
     act = pf[ptr]
     if act["t"] == "number":
@@ -263,28 +303,47 @@ def eval_polish_form(pf):
       args.append(value)
     else:
       print(f"  ERR: ptr={ptr} -> type={act['t']}")
+      raise MalformedExpression
     print(f"  ptr={ptr} -> args={args}")
     ptr += 1
   
+  print(f"eval_polish_form :: args[0]={args[0]}")
   return args[0]
 
-def eval_stack():
+def eval_symlist():
   if len(symlist) == 0:
     return {"state": "empty", "value": None}
     
   if len(symlist) == 1:
     if symlist[-1]["t"] == "number":
-      return {"state": "ok", "value": float(symlist[-1]["s"])}
+      return {"state": "ok", "value": symlist[-1]["s"]}
     else:
-      return {"state": "error", "value": None}
+      return {"state": "malformed expression", "value": None}
     
   # transform to polish form and evaluate
-  polform = to_polish_form(symlist)
-  
-  if error == 1 or scop[-1]["t"] != "number":
-    return {"state": "error", "value": None}
-  else:
-    return {"state": "ok", "value": float(scop[-1]["s"])}
+  val = None
+  error = ""
+  try:
+    pf = to_polish_form(symlist)
+    print(f"pf: {symlist_to_string(pf)}")
+    val = eval_polish_form(pf)
+  except ZeroDivisionError:
+    val = None
+    error = "unexpected value during evaluation"
+  except ValueError:
+    val = None
+    error = "unexpected value during evaluation"
+  except IndexError:
+    val = None
+    error = "malformed expression"
+  except MalformedExpression:
+    val = None
+    error = "malformed expression"
+  finally:
+    if error != "":
+      return {"state": error, "value": None}
+    else:
+      return {"state": "ok", "value": val}
   
 def calc_state():
   global state
@@ -292,7 +351,7 @@ def calc_state():
   if len(symlist) == 0:
     enable_symbol(["OB","SQ","LN","MI"] + all_digs)
     disable_symbol(["CB"] + all_ops)
-    print("  calc_state: stack is empty")
+    # print("  calc_state: stack is empty")
     return
     
   # max number of operators reached => disable further operators, functions and opening brackets
@@ -304,26 +363,34 @@ def calc_state():
     tpt = last["t"]
     tpk = last["k"]
     if tpt == "number":
-      enable_symbol(symbols.keys())
-      print("  calc_state: everything enabled")
+      # enable operators + functions + closing bracket
+      enable_symbol(["CB","EQU"] + all_ops + all_fns)
+      disable_symbol(["OB"])
     elif tpt == "fn" or tpt == "inop":
       enable_symbol(["OB"] + all_digs + all_fns)
-      disable_symbol(["CB"] + all_ops)
+      disable_symbol(["CB","EQU"] + all_ops)
     elif tpk == "CB":
-      disable_symbol(["OB"] + all_digs + all_fns)
-      enable_symbol(["CB"] + all_ops)
+      # no immediate opening bracket or number may follow
+      disable_symbol(["OB"] + all_digs)
+      enable_symbol(["CB","EQU"] + all_ops + all_fns)
+    elif tpk == "OB":
+      # all symbols enabled except for closing bracket
+      enable_symbol(["OB"] + all_ops + all_fns + all_digs)
+      disable_symbol(["OB","EQU"])
   
   # special care for closing brackets: enable only when at least one is open
-  if len(brackets) > 0 and tpk != "OB":
-    state["CB"] = True
-    #print("CB -> True")
-  else:
-    state["CB"] = False
+  # --> finally omitted since we want to enable user to post-bracket the entire expression
+  #if len(brackets) > 0 and tpk != "OB":
+  #  state["CB"] = True
+  #  #print("CB -> True")
+  #else:
+  #  state["CB"] = False
     #print("CB -> False")
   
   # special care for decimal separator
-  if dccount > 0:
-     disable_symbol(["DC"])
+  # --> finally delegated to the front-end
+  #if dccount > 0:
+  #   disable_symbol(["DC"])
   
   
   
@@ -338,6 +405,7 @@ def print_state():
         e.append(k)
       else:
         d.append(k)
+  print(f"EXPR: {state['EXPRESSION']} --> {state['RESULT']} --> state: {state['STATE']}")
   print(f"enabled: {','.join(e)} , disabled: {','.join(d)}")
 
 #tests
@@ -376,7 +444,169 @@ def tst_polform():
   val = eval_polish_form(pf)
   print(f"val: {val}")
 
-tst_polform()
+def tst_divbyzero():
+  lst = [ {"k":"NUMBER","p":0,"s":"1","t":"number"}
+         ,symbols["DI"]
+         ,{"k":"NUMBER","p":0,"s":"0","t":"number"}
+        ]
+    
+  val = None
+  error = ""
+  print(f"orig: {symlist_to_string(lst)}")
+  pf = to_polish_form(lst)
+  print(f"pf: {symlist_to_string(pf)}")
+  try:
+    val = eval_polish_form(pf)
+  except ZeroDivisionError:
+    val = None
+    error = "unexpected value during evaluation"
+  except ValueError:
+    val = None
+    error = "unexpected value during evaluation"
+  except IndexError:
+    val = None
+    error = "malformed expression"
+  
+  print(f"val: {val}, error: {error}")
+
+def tst_malformed1():
+  lst = [ symbols["OB"]
+         ,{"k":"NUMBER","p":0,"s":"1","t":"number"}
+         ,symbols["PL"]
+         ,symbols["CB"]
+        ]
+    
+  val = None
+  error = ""
+  print(f"orig: {symlist_to_string(lst)}")
+  pf = to_polish_form(lst)
+  print(f"pf: {symlist_to_string(pf)}")
+  try:
+    val = eval_polish_form(pf)
+  except ZeroDivisionError:
+    val = None
+    error = "unexpected value during evaluation"
+  except ValueError:
+    val = None
+    error = "unexpected value during evaluation"
+  except IndexError:
+    val = None
+    error = "malformed expression"
+  
+  print(f"val: {val}, error: {error}")
+
+def tst_malformed2():
+  lst = [ symbols["CB"]
+         ,{"k":"NUMBER","p":0,"s":"1","t":"number"}
+         ,symbols["PL"]
+         ,{"k":"NUMBER","p":0,"s":"2","t":"number"}
+        ]
+    
+  val = None
+  error = ""
+  print(f"orig: {symlist_to_string(lst)}")
+  pf = to_polish_form(lst)
+  print(f"pf: {symlist_to_string(pf)}")
+  try:
+    val = eval_polish_form(pf)
+  except ZeroDivisionError:
+    val = None
+    error = "unexpected value during evaluation"
+  except ValueError:
+    val = None
+    error = "unexpected value during evaluation"
+  except IndexError:
+    val = None
+    error = "malformed expression"
+  except MalformedExpression:
+    val = None
+    error = "malformed expression"
+  
+  print(f"val: {val}, error: {error}")
+
+def tst_malformed3():
+  lst = [ {"k":"NUMBER","p":0,"s":"1","t":"number"}
+         ,symbols["PL"]
+         ,{"k":"NUMBER","p":0,"s":"2","t":"number"}
+         ,symbols["CB"]
+        ]
+    
+  val = None
+  error = ""
+  print(f"orig: {symlist_to_string(lst)}")
+  try:
+    pf = to_polish_form(lst)
+    print(f"pf: {symlist_to_string(pf)}")
+    val = eval_polish_form(pf)
+  except ZeroDivisionError:
+    val = None
+    error = "unexpected value during evaluation"
+  except ValueError:
+    val = None
+    error = "unexpected value during evaluation"
+  except IndexError:
+    val = None
+    error = "malformed expression"
+  except MalformedExpression:
+    val = None
+    error = "malformed expression"
+  
+  print(f"val: {val}, error: {error}")
+ 
+ 
+def tst_addsym1():
+  reset()
+  
+  lst = [{"sym":"NUMBER","numliteral":"12.3"}
+        ,{"sym":"PL","numliteral":None}
+        ,{"sym":"NUMBER","numliteral":"2.7"}
+        ]
+        
+  for e in lst:
+    add_symbol(e["sym"], e["numliteral"])
+    
+  print_state()
+  
+def tst_addsym2():
+  reset()
+  
+  lst = [{"sym":"NUMBER","numliteral":"12.3"}
+        ,{"sym":"PL","numliteral":None}
+        ,{"sym":"NUMBER","numliteral":"3.7"}
+        ,{"sym":"SQ","numliteral":None}
+        ]
+        
+  for e in lst:
+    add_symbol(e["sym"], e["numliteral"])
+    
+  print_state()
+  
+def tst_addsym3():
+  reset()
+  
+  lst = [{"sym":"NUMBER","numliteral":"12.3"}
+        ,{"sym":"PL","numliteral":None}
+        ,{"sym":"NUMBER","numliteral":"3.7"}
+        ,{"sym":"CB","numliteral":None}
+        ,{"sym":"MU","numliteral":None}
+        ,{"sym":"NUMBER","numliteral":"2"}
+        ]
+        
+  for e in lst:
+    add_symbol(e["sym"], e["numliteral"])
+    
+  print_state()
+    
+    
+tst_addsym3()
+#tst_addsym2()
+#tst_addsym1()
+         
+#tst_malformed3()
+#tst_malformed2()
+#tst_malformed1()
+#tst_divbyzero()
+#tst_polform()
 
 #reset()
 #add_symbol("OB")
